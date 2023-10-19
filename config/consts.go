783b410e25a22576a40dec68249d5602eee0df50
Arun Kumar <arunkumar@fastly.com>
2023-10-18 23:57:18 -0400

    Better error handling with retrys, and additional logging

--- config/consts.go
+++ config/consts.go
@@ -1,6 +1,8 @@
 package config
 
 const (
-	KV_STORE_NAME   = "hibp_filters"
-	METRO_HASH_SEED = 1337
+	KV_STORE_NAME                       = "hibp_filters"
+	METRO_HASH_SEED                     = 1337
+	MAX_RETRY_FOR_INVALID_200_RESPONSES = 5
+	MAX_RETRY_FOR_HTTP_CLIENT           = 5
 )
--- hibp/verify.go
+++ hibp/verify.go
@@ -3,19 +3,24 @@ package hibp
 import (
 	"compute-hibp-filter/config"
 	"compute-hibp-filter/store"
+	"log"
 
 	"github.com/dgryski/go-metro"
 )
 
 func IsPasswordCompromised(hash string) (bool, error) {
+	log.Println("Verifying password compromise status for hash: ", hash)
 	prefix := hash[:3]
+	log.Println("Retrieving filter from KV store using key:", prefix)
 	filter_bytes, err := store.GetFilterBytesFromStore(prefix)
 	if err != nil {
 		return false, err
 	}
+	log.Println("Retrieved filter of size:", len(filter_bytes), "bytes. Decoding filter...")
 	filter := DecodeXORFilter(filter_bytes)
 	hash_uint64 := metro.Hash64([]byte(hash), config.METRO_HASH_SEED)
 	result := filter.Contains(hash_uint64)
+	log.Println("Looking up metro hash of password hash in filter. Is hash in filter:", result)
 
 	return result, nil
 }
